8d730a394e16401eccb9c9d7dc9d45787da6d542
VyglD <VyglD@yandex.ru>
2020-12-24 23:30:45 +0300

    Placement block has been added

--- source/js/index.js
+++ source/js/index.js
@@ -3,6 +3,7 @@ import {init as initCost} from "./modules/cost";
 import {init as initAutopark} from "./modules/autopark";
 import {init as initReviews} from "./modules/reviews";
 import {init as initCallback} from "./modules/callback";
+import {init as initMap} from "./modules/map";
 
 const NO_JS_MODIFICATOR = `--no-js`;
 
@@ -28,3 +29,4 @@ initCost();
 initAutopark();
 initReviews();
 initCallback();
+initMap();
---
+++ source/js/modules/map.js
@@ -0,0 +1,65 @@
+const MapScript = document.createElement(`script`);
+MapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDdxiZ8JD-Awc3VOchAbkcA54-XHZJYkGY&callback=initMap`;
+MapScript.defer = true;
+
+const getCoordinates = () => {
+  if (window.innerWidth < 760) {
+    return {
+      lat: 55.72408059371243,
+      lng: 37.710367998416714,
+    };
+  }
+
+  return {
+    lat: 55.72408059371243,
+    lng: 37.3,
+  };
+};
+
+const initMap = () => {
+  // eslint-disable-next-line no-undef
+  const map = new google.maps.Map(document.getElementById(`map`), {
+    zoom: 10,
+    center: {
+      lat: 55.72408059371243,
+      lng: 37.710367998416714,
+    },
+  });
+
+  // eslint-disable-next-line no-undef, no-unused-vars
+  const marker = new google.maps.Marker({
+    position: {
+      lat: 55.72408059371243,
+      lng: 37.710367998416714,
+    },
+    map,
+  });
+
+  window.addEventListener(`resize`, () => {
+    map.setCenter(getCoordinates());
+  });
+};
+
+const createMap = (container) => {
+  const wrapper = document.createElement(`div`);
+
+  wrapper.innerHTML = `<div class="placement__map" id="map"></div>`;
+
+  container.replaceChildren(wrapper.firstElementChild);
+
+  window.initMap = initMap;
+
+  document.head.appendChild(MapScript);
+};
+
+const init = () => {
+  const mapWrapper = document.querySelector(`.placement__map-wrapper`);
+
+  if (mapWrapper) {
+    createMap(mapWrapper);
+  }
+};
+
+export {
+  init,
+};
